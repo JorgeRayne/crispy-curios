114b7e16dd308826992184c6dabf1920c75a71d0
Jorge <jorgeraynedavid.iswims@gmail.com>
2026-03-24 07:00:30 +0800

    router-method

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ import http from 'http'
 const port = process.env.PORT || 8000
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.writeHead(200, {'Content-Type' : 'text/html'})
-        res.end('<h1>Home Page</h1>')
-    } else if(req.url === '/about') {
-        res.writeHead(200, {'Content-Type' : 'text/html'})
-        res.end('<h1>About</h1>')
-    } else {
-        res.writeHead(500, {'Content-Type' : 'text/html'})
-        res.end('<h1>Page not found</h1>')
+    try{
+        if(req.method === 'GET'){
+            if(req.url === '/') {
+                res.writeHead(200, {'Content-Type' : 'text/html'})
+                res.end('<h1>Home Page</h1>')
+            } else if(req.url === '/about') {
+                res.writeHead(200, {'Content-Type' : 'text/html'})
+                res.end('<h1>About</h1>')
+            } else {
+                res.writeHead(404, {'Content-Type' : 'text/html'})
+                res.end('<h1>Page not found</h1>')
+            }
+        } else {
+            throw new Error('Method not allow')
+        }
+    } catch (error){
+        res.writeHead(500, {'Content-Type' : 'text/plain'})
+        res.end('<h1>Server Error</h1>')
     }
 })
 
