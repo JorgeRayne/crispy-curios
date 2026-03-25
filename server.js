69b793cb7ca3bfb10c602d369697282f48ad90cf
JorgeRayne <djorgerayne@gmail.com>
2026-03-25 09:51:20 +0800

    merging error fix

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from 'http'
+import getPost from './postController.js'
+const port = process.env.PORT || 8000
 
-const port = process.env.PORT
-
-const server = http
-server.createServer((req, res) => {
-
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type' : 'text/html'})
+    res.end(JSON.stringify(getPost()))
+    // res.write('hello word')
+    // res.end()
 })
 
 server.listen(port)
