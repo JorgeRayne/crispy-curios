6265835baad58bf0bc00adb48226bb87fb115b74
JorgeRayne <djorgerayne@gmail.com>
2026-03-27 19:39:16 +0800

    sample api done

--- server2.js
+++ server2.js
@@ -9,7 +9,25 @@ const users = [
 const server = http.createServer((req, res) => {
     if(req.url == '/api/users'){
         res.setHeader('Content-Type', 'application/json')
-        res.write(JSON.stringify(users))
+        res.write(JSON.stringify(users));
+        res.end()
+    } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3]
+        const user = users.find(user => user.id === parseInt(id))
+        res.setHeader('Content-Type', 'application/json')
+        if (user) {
+            res.write(JSON.stringify(user));
+        } else {
+            res.statusCode = 404
+            res.write(JSON.stringify({message: 'user not found'}));
+        }
+        res.end()
+    } 
+    
+    else {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode(404)
+        res.write(JSON.stringify({message: 'route not found'}))
         res.end()
     }
 })
